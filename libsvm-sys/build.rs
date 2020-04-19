b4dfe6286a2d924f62c2f0881d56b066753dbe84
jerry73204 <jerry73204@gmail.com>
2020-04-20 03:22:09 +0800

    Bugfix: make sure cc invoke c++ compiler

--- libsvm-sys/build.rs
+++ libsvm-sys/build.rs
@@ -1,6 +1,8 @@
 fn main() {
     cc::Build::new()
+        .include("libsvm")
         .file("libsvm/svm.cpp")
+        .cpp(true)
         .flag("-std=c++11")
-        .compile("libsvm.a");
+        .compile("libsvm");
 }
